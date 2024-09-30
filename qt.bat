c328672e5477e5ecae59c62e792b0f29f2fa3361
mandeeps-cloud <69319531+mandeeps-cloud@users.noreply.github.com>
2024-09-30 16:47:45 +0530

    Update qt.bat

--- qt.bat
+++ qt.bat
@@ -2,16 +2,18 @@
 setlocal
 
 :: Set variables
-set QT_INSTALLER_URL=https://download.qt.io/official_releases/qt/6.7/6.7.2/qt-opensource-windows-x86-6.7.2.exe
-set QT_INSTALLER_NAME=qt-opensource-windows-x86-6.7.2.exe
+set QT_INSTALLER_URL=https://download.qt.io/official_releases/online_installers/qt-unified-windows-x64-online.exe
+set QT_INSTALLER_NAME=qt-unified-windows-x64-online.exe
 set INSTALL_DIR=C:\Qt
 set SCRIPT_PATH=%TEMP%\qt_install_script.qs
+set QT_VERSION=6.7.2
+set QT_COMPONENT=qt.qt6.672.win64_msvc2019_64
 
-:: Download Qt installer
-echo Downloading Qt 6.7.2 Installer...
+:: Download the Qt Online Installer
+echo Downloading Qt Online Installer...
 powershell -Command "Invoke-WebRequest -Uri %QT_INSTALLER_URL% -OutFile %QT_INSTALLER_NAME%"
 if %ERRORLEVEL% neq 0 (
-    echo Failed to download the Qt installer. Exiting.
+    echo Failed to download the Qt Online Installer. Exiting.
     exit /b 1
 )
 
@@ -21,20 +23,20 @@ echo Creating the silent installation script...
 echo function Controller() {
 echo     installer.autoAcceptLicense = true;
 echo     installer.setInstallationDirectory("%INSTALL_DIR%");
-echo     installer.addOperation("ComponentSelection", "qt.qt6.%ARCHITECTURE%.6.7.2");
+echo     installer.addOperation("ComponentSelection", "%QT_COMPONENT%");
 echo     installer.setRunMode(installer.RunModeSilent);
 echo }
 ) > %SCRIPT_PATH%
 
 :: Run the Qt installer silently
-echo Running the Qt installer silently...
-start /wait %QT_INSTALLER_NAME% --silent --script %SCRIPT_PATH%
+echo Running the Qt Online Installer silently...
+start /wait %QT_INSTALLER_NAME% --silent --platform minimal --script %SCRIPT_PATH%
 
 :: Check if Qt was installed successfully
-if exist "%INSTALL_DIR%\6.7.2" (
-    echo Qt 6.7.2 installation completed successfully.
+if exist "%INSTALL_DIR%\%QT_VERSION%" (
+    echo Qt %QT_VERSION% installation completed successfully.
 ) else (
-    echo Qt 6.7.2 installation failed.
+    echo Qt %QT_VERSION% installation failed.
     exit /b 1
 )
 
